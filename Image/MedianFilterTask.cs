1fed9a3b1ac9621324f9c70565a63c1f0f242ff8
TimurFromCanada <timurfromcanada@gmail.com>
2020-11-27 11:41:52 +0500

    Add files via upload

---
+++ Image/MedianFilterTask.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Recognizer
+{
+    internal static class MedianFilterTask
+    {
+        static double GetMedianInList(List<double> list)
+        {
+            list.Sort();
+            var count = list.Count;
+            if (count % 2 == 0)
+                return (list[count / 2 - 1] + list[count / 2]) / 2;
+            return list[count / 2];
+        }
+
+        static void ValueMedian(int i, int j, List<double> list, double[,] original)
+        {
+            var column = original.GetLength(1);
+
+            if (j - 1 > -1)
+                list.Add(original[i, j - 1]);
+            list.Add(original[i, j]);
+            if (j + 1 < column)
+                list.Add(original[i, j + 1]);
+        }
+
+        static List<double> ArrayInList(int i, int j, double[,] original)
+        {
+            var row = original.GetLength(0);
+            var list = new List<double>();
+
+            if (i - 1 > -1)
+                ValueMedian(i - 1, j, list, original);
+            ValueMedian(i, j, list, original);
+            if (i + 1 < row)
+                ValueMedian(i + 1, j, list, original);
+            return list;
+        }
+
+        public static double[,] MedianFilter(double[,] original)
+        {
+            var row = original.GetLength(0);
+            var column = original.GetLength(1);
+            var arr = new double[row, column];
+
+            for (var i = 0; i < row; i++)
+                for (var j = 0; j < column; j++)
+                    arr[i, j] = GetMedianInList(ArrayInList(i, j, original));
+            return arr;
+        }
+    }
+}
---
+++ Image/ThresholdFilterTask.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System;
+namespace Recognizer
+{
+    public static class ThresholdFilterTask
+    {
+        static double GetBrightnessThreshold(double[,] original, double threshold)
+        {
+            var row = original.GetLength(0);
+            var column = original.GetLength(1);
+            var numberWhite = Convert.ToInt32(Math.Floor(threshold * row * column));
+            var list = new List<double>();
+
+            for (var i = 0; i < row; i++)
+                for (var j = 0; j < column; j++)
+                    list.Add(original[i, j]);
+            list.Sort();
+
+            if (numberWhite == 0)
+                return list[row * column - 1] + 1;
+            return list[row * column - numberWhite];
+        }
+
+        public static double[,] ThresholdFilter(double[,] original, double threshold)
+        {
+            var brightnessThreshold = GetBrightnessThreshold(original, threshold);
+            var row = original.GetLength(0);
+            var column = original.GetLength(1);
+            var arr = new double[row, column];
+            for (var i = 0; i < row; i++)
+                for (var j = 0; j < column; j++)
+                    if (original[i, j] >= brightnessThreshold)
+                        arr[i, j] = 1;
+            return arr;
+        }
+    }
+}
